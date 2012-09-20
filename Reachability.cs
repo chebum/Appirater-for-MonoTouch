b94379411beeb94a136b45789099708516a94abd
chebum <cherniy.bumer@gmail.com>
2012-09-20 11:13:52 +0400

    Solved warnings when compiling for MonoTouch 6.0

--- Reachability.cs
+++ Reachability.cs
@@ -71,7 +71,7 @@ public static class Reachability
 	{
 		if (adHocWiFiNetworkReachability == null) {
 			adHocWiFiNetworkReachability = new NetworkReachability (new IPAddress (new byte [] {169,254,0,0}));
-			adHocWiFiNetworkReachability.SetCallback (OnChange);
+			adHocWiFiNetworkReachability.SetNotification (OnChange);
 			adHocWiFiNetworkReachability.Schedule (CFRunLoop.Current, CFRunLoop.ModeDefault);
 		}
 		
@@ -87,7 +87,7 @@ public static class Reachability
 	{
 		if (defaultRouteReachability == null) {
 			defaultRouteReachability = new NetworkReachability (new IPAddress (0));
-			defaultRouteReachability.SetCallback (OnChange);
+			defaultRouteReachability.SetNotification (OnChange);
 			defaultRouteReachability.Schedule (CFRunLoop.Current, CFRunLoop.ModeDefault);
 		}
 		if (defaultRouteReachability.TryGetFlags (out flags))
@@ -109,7 +109,7 @@ public static class Reachability
 			// this only happens when you create NetworkReachability from a hostname
 			reachable = remoteHostReachability.TryGetFlags (out flags);
 			
-			remoteHostReachability.SetCallback (OnChange);
+			remoteHostReachability.SetNotification (OnChange);
 			remoteHostReachability.Schedule (CFRunLoop.Current, CFRunLoop.ModeDefault);
 		} else
 			reachable = remoteHostReachability.TryGetFlags (out flags);			
